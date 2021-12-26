ee9526c5275ae162dc984fae8f91073591ce3a41
tahsinapriya <tahsinapriya@gmail.com>
2021-12-26 20:18:36 +0600

    Add initial project

---
+++ src/main/java/com/priyapritty/survey/SurveyApplication.java
@@ -0,0 +1,20 @@
+package com.priyapritty.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyApplication.class, args);
+	}
+    @GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+}
+
