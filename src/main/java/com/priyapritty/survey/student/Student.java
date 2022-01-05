4fea05918f0707b2cdd18a6adad6ca2c616b131a
tahsinapriya <tahsinapriya@gmail.com>
2022-01-05 23:07:07 +0600

    Create and get student from db

--- src/main/java/com/priyapritty/survey/student/Student.java
+++ src/main/java/com/priyapritty/survey/student/Student.java
@@ -1,30 +1,44 @@
 package com.priyapritty.survey.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private long id;
     private String name;
     private String email;
     private LocalDate dob;
+
+    @Transient
     private Integer age;
 
     public Student() {
     }
 
-    public Student(long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public long getId() {
@@ -60,7 +74,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
@@ -74,7 +88,7 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", dob=" + dob +
-                ", age=" + age +
+                ", age=" + getAge() +
                 '}';
     }
 }
--- src/main/java/com/priyapritty/survey/student/StudentService.java
+++ src/main/java/com/priyapritty/survey/student/StudentService.java
@@ -1,8 +1,7 @@
 package com.priyapritty.survey.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -11,8 +10,13 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return Arrays.asList(new Student(1L, "mariam","mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY,5),21));
+        return studentRepository.findAll();
     }
 }
