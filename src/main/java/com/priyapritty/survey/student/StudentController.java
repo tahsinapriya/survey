22bd1ed56918b12a68183d2cf0ab4012151d41a1
tahsinapriya <tahsinapriya@gmail.com>
2022-01-09 22:13:45 +0600

    Add student update api

--- src/main/java/com/priyapritty/survey/student/StudentController.java
+++ src/main/java/com/priyapritty/survey/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.priyapritty.survey.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +18,22 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student) {
+        Student student1 = studentService.findStudentByEmail(student.getEmail()).get();
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId,name,email);
 
     }
+
+
 }
 
 
--- src/main/java/com/priyapritty/survey/student/StudentService.java
+++ src/main/java/com/priyapritty/survey/student/StudentService.java
@@ -3,14 +3,18 @@ package com.priyapritty.survey.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -18,5 +22,26 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public Optional<Student> findStudentByEmail(String email) {
+        return studentRepository.findStudentByEmail(email);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + "does not exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student>studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
