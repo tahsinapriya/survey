b9d60f1d9ca3da9f7602bf5e731b63feb3f4b38c
tanzinapritty <tanzinapritty98@gmail.com>
2022-01-12 22:17:46 +0600

    Implement question_type get method

--- src/main/java/com/priyapritty/survey/question_type/QuestionTypeController.java
+++ src/main/java/com/priyapritty/survey/question_type/QuestionTypeController.java
@@ -1,4 +1,28 @@
 package com.priyapritty.survey.question_type;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path= "api/v1/question_type")
 public class QuestionTypeController {
+    private final QuestionTypeService questionTypeService;
+
+    @Autowired
+    public QuestionTypeController(QuestionTypeService questionTypeService) {
+        this.questionTypeService = questionTypeService;
+    }
+
+    @GetMapping
+    public List<QuestionType> getQuestionType() {
+        return questionTypeService.getQuestionType();
+
+    }
 }
+
+
+
--- src/main/java/com/priyapritty/survey/question_type/QuestionTypeService.java
+++ src/main/java/com/priyapritty/survey/question_type/QuestionTypeService.java
@@ -1,4 +1,24 @@
 package com.priyapritty.survey.question_type;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class QuestionTypeService {
+    private final QuestionTypeRepository questionTypeRepository;
+
+    @Autowired
+    public QuestionTypeService(QuestionTypeRepository questionTypeRepository) {
+        this.questionTypeRepository = questionTypeRepository;
+    }
+
+    public List<QuestionType> getQuestionType() {
+        return questionTypeRepository.findAll();
+    }
 }
+
