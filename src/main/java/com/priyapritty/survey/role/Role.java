4b9c7c19ea12aaf1c445511f4f654fa417b78f46
tahsinapriya <tahsinapriya@gmail.com>
2022-01-18 18:17:01 +0600

    Fix role sequence and add setters/getters

--- src/main/java/com/priyapritty/survey/role/Role.java
+++ src/main/java/com/priyapritty/survey/role/Role.java
@@ -7,14 +7,42 @@ import javax.persistence.*;
 public class Role {
     @Id
     @SequenceGenerator(
-            name = "users_sequence",
-            sequenceName = "users_sequence",
+            name = "role_sequence",
+            sequenceName = "role_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "users_sequence"
+            generator = "role_sequence"
     )
     private long id;
     private String name;
+
+    public Role() {
+    }
+
+    public Role(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
